ee700cc0b543f439f54d91c11af64272b6d1825d
Said Aroua <said.aroua@bluewin.ch>
2024-08-19 17:59:08 -0700

    Add sample page /events

---
+++ RazorTemplate/Pages/Events.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorTemplate.Db;
+using RazorTemplate.Db.Entities;
+
+namespace RazorTemplate.Pages;
+
+public class Events : PageModel
+{
+    private readonly DataContext _context;
+
+    public Events(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public List<Event> AllEvents { get; set; } = [];
+
+
+    public void OnGet()
+    {
+        AllEvents = _context.Events.Include(p => p.Attendees).ToList();
+    }
+}
